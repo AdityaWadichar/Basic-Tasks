9ce0d3029a4cc29ab88207fb26665ed622cb72c7
Aditya Wadichar <adityawadichar85@gmail.com>
2020-10-14 00:23:30 +0530

    Update simple pendulum.m

--- simple pendulum.m
+++ simple pendulum.m
@@ -2,9 +2,9 @@ clear
 clc
 b=0.5; g=9.8; L=1.5;
 ini_pos=0;
-ini_vel=2.5;
+ini_vel=3;
 T=2000;
-t=0.01;
+t=0.1;
 theta=zeros(1, T);
 omega=zeros(1, T);
 theta(1)=ini_pos;
@@ -14,16 +14,14 @@ for i=1:T-1
     omega(i+1)=(theta(i+1)-theta(i))/t;
 end
 
-subplot(2,1,1)
-%axis([0 20 -4 4])
-plot([1:T]*t, omega, '.b')
-xlabel('Time');
-ylabel('angular velocity')
 
 
 for k=1:T
     
-
+subplot(2,1,1)
+axis([0 20 -4 4])
+plot(k*t, omega(k), 'b.')
+hold on
 subplot(2,1,2)
 axis(gca, 'equal')
 axis([-2 2 -2 2])
@@ -31,7 +29,8 @@ axis([-2 2 -2 2])
     crack= line([0 L*sin(theta(k))], [0 -L*cos(theta(k))]);
     circ1= viscircles([0 0], 0.05);
     circ2= viscircles([L*sin(theta(k)) -L*cos(theta(k))], 0.05);
-    pause(0.0000001);
-    delete([crack, circ1, circ2]);
-
+    pause(0.067);
+    delete(crack);
+    delete(circ1);
+    delete(circ2);
 end
